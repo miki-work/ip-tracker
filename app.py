7642c14c642bb1d025983a90adf52aa475d7142d
miki <mikiwork723@gmail.com>
2025-10-26 16:28:55 +0500

    change port

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def view_clicks():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
